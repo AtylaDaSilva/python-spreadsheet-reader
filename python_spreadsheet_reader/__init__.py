169512afc501e5bb2863cc8244f0a8c12faa9928
AtylaDaSilva <atylamendes97@gmail.com>
2026-07-23 16:37:46 -0300

    chore: moved reader bundled imports

--- python_spreadsheet_reader/__init__.py
+++
@@ -1,6 +0,0 @@
-from .readers.xlsx import XLSXReader
-from .readers.exceptions import (
-    SpreadsheetReaderException,
-    SpreadsheetIsLockedException,
-    NoActiveSpreadsheetException,
-)
---
+++ python_spreadsheet_reader/readers/__init__.py
@@ -0,0 +1,6 @@
+from .xlsx import XLSXReader
+from .exceptions import (
+    SpreadsheetReaderException,
+    SpreadsheetIsLockedException,
+    NoActiveSpreadsheetException,
+)
